87090cda737007324e031d1325cfa6cc734d5c49
Atkinss1 <jordanatkins@live.com>
2023-08-06 17:22:35 -0600

    Added return and console-log examples to hello.js. Added isEven function in even.js

--- hello.js
+++ hello.js
@@ -6,3 +6,17 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
 
+//console.log
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+// return
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
