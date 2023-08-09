67c6f94b1c8d05f824c18513fc62f0d1698a3beb
Atkinss1 <jordanatkins@live.com>
2023-08-08 19:43:29 -0600

    added variable i to code for while loop

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+let i =0;
+
 console.log("Kitchen stuff to pack:");
 while(i < packingList.length) {
   console.log(packingList[i]);
